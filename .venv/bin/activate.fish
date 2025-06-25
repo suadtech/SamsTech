54806a037539f92f25ad663610fe6a56e636a56a
suadtech <suadtech1@gmail.com>
2025-06-25 22:34:44 +0000

    update products

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/workspaces/Sam-sTech/.venv"
+set -gx VIRTUAL_ENV "/workspaces/SamsTech/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
