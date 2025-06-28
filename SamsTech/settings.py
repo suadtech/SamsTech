c3457fc4b1701028400a5704919abcc26bbcb853
suad alhodifi <suadtech1@gmail.com>
2025-06-28 00:43:57 +0000

    update productes

--- SamsTech/settings.py
+++ SamsTech/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9*tgqt+d+50@xh=^bv$@k6prp^k0q0=$@t$d*%q%)=qx8=2f5d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
