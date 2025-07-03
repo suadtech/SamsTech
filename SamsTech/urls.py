7e4cd26872233ddaef3046587776942d8c964870
suad alhodifi <suadtech1@gmail.com>
2025-07-03 21:43:06 +0000

    update

--- SamsTech/urls.py
+++ SamsTech/urls.py
@@ -27,3 +27,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -6,3 +6,5 @@ def index(request):
     """ A view to return the index page """
     
     return render(request, 'home/index.html')
+def test_static_view(request): 
+    return render(request, 'test_static.html')
