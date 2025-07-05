4053bb89a17af7e67d670b74afa06139e5a1107f
suad alhodifi <suadtech1@gmail.com>
2025-07-05 02:37:49 +0000

    update

--- bag/views.py
+++ bag/views.py
@@ -1,28 +1,65 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
-from products.models import Product # Import the Product model
+from products.models import Product
 from django.contrib import messages # Import messages
 
 # Create your views here.
+
 def view_bag(request):
-    """A view that renders the bag contents page"""
+    """ A view that renders the bag contents page """
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
-    """Add a specified quantity of the product to the shopping bag"""
+    """ Add a specified quantity of the product to the bag """
 
-    product = get_object_or_404(Product, pk=item_id) # Get the product object
-    quantity = int(request.POST.get('quantity')) # Get quantity from form
-    redirect_url = request.POST.get('redirect_url') # Get redirect URL from form (optional)
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url') # This line is not used in your provided code, but kept for consistency if you use it later
 
-    bag = request.session.get('bag', {}) # Get the bag from session, or create an empty dict
+    bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
-        bag[item_id] += quantity # If item already in bag, increment quantity
+        bag[item_id] += quantity
         messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
-        bag[item_id] = quantity # Add new item to bag
+        bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
+    
+    request.session['bag'] = bag
+    # print(request.session['bag']) # For debugging, you can uncomment this
+    return redirect(redirect_url) # Redirect to the URL provided in the form
 
-    request.session['bag'] = bag # Update the session bag
 
-    return redirect(reverse('products:product_detail', args=[item_id])) # Redirect back to product detail
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+    else:
+        bag.pop(item_id)
+        messages.info(request, f'Removed {product.name} from your bag')
+
+    request.session['bag'] = bag
+    return redirect(reverse('bag:view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        bag = request.session.get('bag', {})
+        bag.pop(item_id)
+        messages.info(request, f'Removed {product.name} from your bag')
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
