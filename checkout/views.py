1e42d7fa35e1598655c59e5ddebfe20c2848c219
suad alhodifi <suadtech1@gmail.com>
2025-07-07 21:33:52 +0000

    update

--- checkout/views.py
+++ checkout/views.py
@@ -23,16 +23,17 @@ def checkout(request):
         bag = request.session.get('bag', {})
         
         form_data = {
-            'full_name': request.POST['full_name'],
-            'email': request.POST['email'],
-            'phone_number': request.POST['phone_number'],
-            'country': request.POST['country'],
-            'postcode': request.POST['postcode'],
-            'town_or_city': request.POST['town_or_city'],
-            'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST['street_address2'],
-            'county': request.POST['county'],
-        }
+    'full_name': request.POST['full_name'],
+    'email': request.POST['email'],
+    'phone_number': request.POST['phone_number'],
+    'country': request.POST['country'],
+    'postcode': request.POST['postcode'],
+    'town_or_city': request.POST['town_or_city'],
+    'street_address1': request.POST['street_address1'],
+    'street_address2': request.POST['street_address2'],
+    'county': request.POST.get('county', ''),  # Use .get() with default empty string
+}
+
         
         order_form = OrderForm(form_data)
         if order_form.is_valid():
