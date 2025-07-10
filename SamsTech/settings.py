a4487e51f50e5b1bff9f6887306712f136bf5290
suad alhodifi <suadtech1@gmail.com>
2025-07-10 06:35:20 +0000

    Fix: Correctly configure WhiteNoise for Heroku

--- SamsTech/settings.py
+++ SamsTech/settings.py
@@ -81,6 +81,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',  
@@ -200,6 +201,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
 
 # Add STATIC_ROOT for collectstatic command
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
