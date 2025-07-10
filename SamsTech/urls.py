c98448bb427fe9800a79efb073814da9f8fabfc9
suad alhodifi <suadtech1@gmail.com>
2025-07-10 06:52:24 +0000

    Fix: Add missing namespace for checkout app

--- SamsTech/urls.py
+++ SamsTech/urls.py
@@ -1,22 +1,15 @@
-
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-   
-   
-
-    
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
-    path('checkout/', include('checkout.urls')),
+    # This is the line you are fixing:
+    path('checkout/', include(('checkout.urls', 'checkout'), namespace='checkout')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
