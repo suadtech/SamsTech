717cde64b56c766af517adee590158b20aa86157
suad alhodifi <suadtech1@gmail.com>
2025-07-10 04:59:04 +0000

    Fix MultiValueDictKeyError by making county optional in OrderForm

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,23 +1,24 @@
 from django import forms
-from django_countries.fields import CountryField
 from .models import Order
 
-class OrderForm(forms.ModelForm):
-    country = CountryField(blank_label='Country *').formfield(
-        required=True,
-        widget=forms.Select(attrs={
-            'class': 'stripe-style-input',
-        })
-    )
 
+class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ('full_name', 'email', 'phone_number', 
-                 'town_or_city', 'street_address1',
-                'street_address2', 'postcode', 'country', 'county',)
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
 
     def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
         super().__init__(*args, **kwargs)
+        
+        # THIS IS THE FIX: Make 'county' not required
+        self.fields['county'].required = False
         
         placeholders = {
             'full_name': 'Full Name',
@@ -32,11 +33,11 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if field != 'country':  # Skip country field
+            if field != 'country':
                 if self.fields[field].required:
-                    placeholder = f'{placeholders.get(field, "")} *'
+                    placeholder = f'{placeholders[field]} *'
                 else:
-                    placeholder = placeholders.get(field, "")
+                    placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
